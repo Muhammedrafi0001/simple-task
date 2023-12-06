0c41a333e328ac64572615947e6d7f3ceda76153
rafi <rafip9962gmail.com>
2023-12-06 17:04:36 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Add from './add';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hellooooo...........................
-        helooooodalskdfjasd 
-        newONe
-      </h1>
+     <Add/>
     </div>
   );
 }
